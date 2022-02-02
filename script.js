334ee41b9e3bbf451b8a4d64e494d5bbcf94c05b
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-02-02 15:51:14 +0200

    second version with changes

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1
+let userName = prompt("What is your name?");
+alert(userName);
+
+// 2
+let numberFirst = +prompt("Enter the first number");
+let numberSecond = +prompt("Enter the second number");
+
+let multiplication = numberFirst * numberSecond;
+let division = numberFirst / numberSecond;
+let subtraction = numberFirst - numberSecond;
+let addition = numberFirst + numberSecond;
+alert(
+  `multiplication result: ${multiplication},
+    division result: ${division},
+    subtraction result: ${subtraction},
+    addition result: ${addition}`
+);
+
+console.log("New-feature");
